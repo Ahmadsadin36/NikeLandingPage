b70fae00b65417eb0efba30d93439833825e4b4a
Ahmadsadin36 <sadinahmad1999@gmail.com>
2024-05-23 19:10:25 +0200

    I create this landing page using reactjs and tailwind css

---
+++ Nike/Nike/src/components/index.js
@@ -0,0 +1,32 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import ShoeCard from "./ShoeCard";
+import PopularProducts from "./PopularProducts";
+import ServiceCard from "./Services";
+import CostumerReviews from "./CostumerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import PopularProductCard from "./PopularProductCard";
+import ReviewCard from "./ReviewCard";
+
+
+export {
+    Button,
+    Nav,
+    ShoeCard,
+    CostumerReviews,
+    Footer,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard
+}
